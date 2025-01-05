a7a68de698390f08797cc47bbc8e84e26b06af65
CookieMunster73 <coldrush73@gmail.com>
2025-01-05 11:06:40 -0700

    added another gen project for comprehension

---
+++ another_gen.py
@@ -0,0 +1,20 @@
+ten = [1,2,3,4,5,6,7,8,9,10]
+
+def topten():
+    global ten
+    ten = ten.__iter__()
+    try:
+        while True:
+            yield next(ten)
+    except StopIteration:
+        print("End of list.")
+
+# values = topten()
+
+# print(next(values))
+# print(next(values))
+# print(next(values))
+# print(next(values))
+
+for num in topten():
+    print(num)
